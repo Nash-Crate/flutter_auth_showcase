bd4043cfa5c152bc16b68d9adcd76fb85988c275
Prageeth Monasha <spmonasha@gmail.com>
2025-08-03 17:47:25 +0200

    [f/login] firebase initialized;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/app.dart';
+import 'package:flutter_auth/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  /// firebase initialization
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const App());
 }
