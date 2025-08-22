609f2756995cf55e6bb8076249cf7195920e6dc5
Prageeth Monasha <spmonasha@gmail.com>
2025-08-22 14:56:46 +0200

    [config] CICD fixing;

--- lib/features/common/core/usecases/usecase.dart
+++ lib/features/common/core/usecases/usecase.dart
@@ -3,38 +3,38 @@
 import 'package:flutter_showcase/features/common/errors/errors.dart';
 import 'package:fpdart/fpdart.dart';
 
-/// Interface for usecase with [Params] and returns [Type]
-abstract interface class Usecase<Type, Params> {
+/// Interface for usecase with [Params] and returns [ReturnType]
+abstract interface class Usecase<ReturnType, Params> {
   /// Call the usecase with params
-  Future<Either<Failure, Type>> call(Params params);
+  Future<Either<Failure, ReturnType>> call(Params params);
 }
 
-/// Interface for usecase with no params and returns [Type]
-abstract interface class UsecaseNoParams<Type> {
+/// Interface for usecase with no params and returns [ReturnType]
+abstract interface class UsecaseNoParams<ReturnType> {
   /// Call the usecase
-  Future<Either<Failure, Type>> call();
+  Future<Either<Failure, ReturnType>> call();
 }
 
-/// Interface for synchronous usecase with no params and returns [Type]
-abstract interface class UsecaseNoParamsSync<Type> {
+/// Interface for synchronous usecase with no params and returns [ReturnType]
+abstract interface class UsecaseNoParamsSync<ReturnType> {
   /// Call the usecase
-  Either<Failure, Type> call();
+  Either<Failure, ReturnType> call();
 }
 
-/// Interface for synchronous usecase with [Params] and returns [Type]
-abstract interface class UsecaseSync<Type, Params> {
+/// Interface for synchronous usecase with [Params] and returns [ReturnType]
+abstract interface class UsecaseSync<ReturnType, Params> {
   /// Call the usecase with params
-  Either<Failure, Type> call(Params params);
+  Either<Failure, ReturnType> call(Params params);
 }
 
-/// Interface for streamed usecase with [Params] and returns [Type]
-abstract interface class UsecaseStream<Type, Params> {
+/// Interface for streamed usecase with [Params] and returns [ReturnType]
+abstract interface class UsecaseStream<ReturnType, Params> {
   /// Call the usecase with params
-  Stream<Either<Failure, Type>> call(Params params);
+  Stream<Either<Failure, ReturnType>> call(Params params);
 }
 
-/// Interface for streamed usecase with no params and returns [Type]
-abstract interface class UsecaseStreamNoParams<Type> {
+/// Interface for streamed usecase with no params and returns [ReturnType]
+abstract interface class UsecaseStreamNoParams<ReturnType> {
   /// Call the usecase
-  Stream<Either<Failure, Type>> call();
+  Stream<Either<Failure, ReturnType>> call();
 }
